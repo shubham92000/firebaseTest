65b407274c4757169077db0c7e5d390b267aa3fa
shubham92000 <sbhsingh616@gmail.com>
2022-02-16 21:24:51 +0530

    added .env file for apikeys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCJZqMng6Pfd55ePGzjJgqYFHep9ynduBU',
-  authDomain: 'fir-testapp-2dcf7.firebaseapp.com',
-  projectId: 'fir-testapp-2dcf7',
-  storageBucket: 'fir-testapp-2dcf7.appspot.com',
-  messagingSenderId: '148546050541',
-  appId: '1:148546050541:web:733bbd4f3c303fb7e7efe4',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
